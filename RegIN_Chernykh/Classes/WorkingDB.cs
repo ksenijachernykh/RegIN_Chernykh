71c74f36afa3ff50fd50a3cd8ae57036a784c14d
Kaktys868 <lastochka1230@yandex.ru>
2025-11-25 17:04:58 +0500

    Доделать класс WorkingDB.Черных

---
+++ RegIN_Chernykh/Classes/WorkingDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegIN_Chernykh.Classes
+{
+    public class WorkingDB
+    {
+        ///<summary>
+        /// Строка подключения к 
+        /// </summary>
+    }
+}
