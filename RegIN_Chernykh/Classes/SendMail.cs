a47ac372183467147e687b59ef3538da6dc16a9a
Kseniya <ksenijachernyh@yandex.ru>
2025-12-09 10:34:03 +0300

    Разработанный класс отправки сообщения на почту. Черных

---
+++ RegIN_Chernykh/Classes/SendMail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegIN_Chernykh.Classes
+{
+    public class SendMail
+    {
+        ///<summary>
+        ///Функция отправки сообщения
+        ///</summary>
+        ///<param name="Message">Сообщение которое необходимо отправить</param>
+        ///<param name="To">Почта на которую отправляется сообщение</param>
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port 587,
+                Credentials = new NetworkCredential("yandexdyandex.ru", "password"),
+                EnableSs1 = true,
+
+            };
+            smtpClient.Send("landaxer@yandex.ru", То, "Проект RegIn", Message);
+        }
+
+    }
+}
